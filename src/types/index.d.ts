f38a0ae005a61de9111352201e4b7dfdbcea6d02
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-03-07 11:22:18 +0700

    chore: fix types entry

---
+++ src/types/index.d.ts
@@ -0,0 +1,6 @@
+export type * from './adapter';
+export type * from './chains';
+export type * from './client';
+export type * from './nfts';
+export type * from './stats';
+export type * from './tokens';
--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export type * from './adapter.d';
-export type * from './chains.d';
-export type * from './client.d';
-export type * from './nfts.d';
-export type * from './stats.d';
-export type * from './tokens.d';
