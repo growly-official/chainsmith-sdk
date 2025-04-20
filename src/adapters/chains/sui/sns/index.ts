55aff1511a4c85a60b320c98f3c0082f5f00e0d8
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-04-20 16:11:03 +0700

    chore: remove sui

--- src/adapters/chains/sui/sns/index.ts
+++
@@ -1,7 +0,0 @@
-import type { IAdapter } from '../../../../types';
-import { Logger } from 'tslog';
-
-export class SuiNameServiceAdapter implements IAdapter {
-  name: string = 'sui.SuiNameServiceAdapter';
-  logger = new Logger({ name: 'MultiPlatformSocialPlugin' });
-}
